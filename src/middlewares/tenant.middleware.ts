cbc21893fd25fe2a65def79858975ce19cdf8e45
Quang Akiyama <quanganhcf2@gmail.com>
2025-09-29 20:33:55 +0700

    add log

--- src/middlewares/tenant.middleware.ts
+++ src/middlewares/tenant.middleware.ts
@@ -5,12 +5,15 @@ import { Request, Response, NextFunction } from 'express';
 export class TenantMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     let tenantId = req.headers['x-tenant-id'] as string;
+    console.log('Tenant ID:', tenantId);
+
     if (!tenantId) {
       tenantId = 'LMS'; // Default tenant ID if not provided
     }
     // Lấy databaseUrl từ config hoặc env
     const databaseUrl = process.env[`DATABASE_URL_${tenantId.toUpperCase()}`];
-    if (!databaseUrl) return res.status(400).json({ message: 'Invalid tenant id' });
+    if (!databaseUrl)
+      return res.status(400).json({ message: 'Invalid tenant id' });
 
     (req as any).databaseUrl = databaseUrl;
     next();
