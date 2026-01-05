a307009aecd73c050c7bef831fe55538fa881096
Akiyama Le <61172912+akiyamahaa@users.noreply.github.com>
2026-01-05 20:55:59 +0700

    Add new allowed origin for CORS

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,7 @@ async function bootstrap() {
       'https://thinklab-2110.vercel.app',
       'https://anh-hung-su-viet.vercel.app',
       'https://math-tek.vercel.app',
+      'https://historyfun.vercel.app'
     ], // Cho phép cả localhost và 127.0.0.1
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Nếu dùng cookie, JWT trong header
